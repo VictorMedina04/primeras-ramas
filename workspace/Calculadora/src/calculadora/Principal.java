65b8f6261e0fdb9755be93551e98fe7af3c6da59
Victor <medina.levic24@triana.salesianos.edu>
2024-04-11 14:16:13 +0200

    menu

--- workspace/Calculadora/src/calculadora/Principal.java
+++ workspace/Calculadora/src/calculadora/Principal.java
@@ -1,10 +1,27 @@
 package calculadora;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		String aux;
+		int op;
+		
+		do {
+			System.out.println("""
+					0-Salir
+					1-Suma
+					2-Resta
+					3-Multiplicar
+					4-Dividir
+					""");
+			aux = sc.nextLine();
+			op = Integer.parseInt(aux);
+			
+		}while(op!=0);
 	}
 
 }
